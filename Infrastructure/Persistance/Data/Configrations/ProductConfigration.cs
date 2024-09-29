4aa52498a6c0ce7e85818dba213b65b93ff37a69
Ahmed <atrees.ahmed5544@gmail.com>
2024-09-29 22:24:45 +0300

    Add project files.

---
+++ Infrastructure/Persistance/Data/Configrations/ProductConfigration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Data.Configrations
+{
+    public class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.Brand)
+                .WithMany(b => b.Products)
+                .HasForeignKey(p => p.BrandId)
+                ; 
+
+            builder.HasOne(p => p.Type)
+                .WithMany(t => t.Products)
+                .HasForeignKey(p => p.TypeId)
+                ; 
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,3)");
+        }
+    }
+}
---
+++ Infrastructure/Persistance/Data/StoreContext.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreContext).Assembly);
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+    }
+}
